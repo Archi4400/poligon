03be38d801c4c737f34ad40705f3bfcfeb525fee
arthur <arthur.lyashenko@dexdgtl.com>
2022-01-31 17:26:50 +0200

    lesson 13| add route and controler for admin blog

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
 
-// обертка
+// Blog
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function() {
     // роуты внутри (можно было каждому роуту прописывать обертку blog/)
     route::resource('posts','PostController')->names('blog.posts');
 });
 
-// Route::resource('rest', 'RestTestController')->names('restTest');
+// Admin panel
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix'    => 'admin/blog',
+];
+Route::group($groupData, function () {
+    // Blog category
+    $methods = ['index', 'edit', 'update', 'create', 'store',];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
